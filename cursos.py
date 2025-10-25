41007c4a8e92e65f49ea5fd7a9e86a087ca331f2
Michael <michaelsb065@gmail.com>
2025-10-25 02:30:10 -0300

    corrigindo exeções

--- cursos.py
+++ cursos.py
@@ -1,10 +1,12 @@
 from db import conectar
 
 def inserir_cursos():
-    conn, cursor = conectar()
 
     nome_curso = input("Nome do curso: ")
+
     try:
+        conn, cursor = conectar()
+
         carga_horaria = int(input("Carga horária do curso: "))
         nivel = input("Nível do curso (Básico, Intermediário, Avançado): ")
         nivel = nivel.lower().replace("á", "a").replace("ç", "c")
@@ -14,11 +16,15 @@ def inserir_cursos():
                               INSERT INTO cursos (nome, carga_horaria, nivel)
                               VALUES (?, ?, ?)
                               """, (nome_curso, carga_horaria, nivel))
+            conn.commit()
+
             print("\033[32mCurso cadastrado com sucesso.\033[0m")
+
+            conn.close()
         else:
             print("\033[31mERRO.\033[0m Nível incorreto. ")
     except ValueError:
         print("\033[31mERRO.\033[0m Carga horária invalída. ")
-
-    conn.commit()
-    conn.close()
+    except Exception as e:
+        print("\033[31mERRO.[0m Não foi possível inserir curso. ", e)
+    
